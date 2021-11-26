9eca520f5d1b3c7cd5060672b10f973e075aabc8
Jose Camata <72111360+josecamata@users.noreply.github.com>
2021-11-26 09:26:55 -0300

    Update hdf5AutoInstall.sh

--- hdf5AutoInstall.sh
+++ hdf5AutoInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~
+
 mkdir tmp_hdf5
 cd tmp_hdf5 
 wget -c -np https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.1/src/hdf5-1.12.1.tar.gz
@@ -21,14 +21,13 @@ sudo make install
 ####
 git clone https://github.com/LLNL/zfp.git 
 cd zfp 
-#cat 'DEFS += -DBIT_STREAM_WORD_TYPE=uint8' >> Config 
+cp ../files/Config .
 
-#Editar Config  descmentando a linha
-#  DEFS += -DBIT_STREAM_WORD_TYPE=uint8.
 make
 sudo mkdir /opt/lib/zfp
-sudo mv include /opt/lib/zfp 
-sudo mv lib /opt/lib/zfp 
+sudo cp -r include /opt/lib/zfp 
+sudo cp -r /opt/lib/zfp
+sudo cp Config /opt/lib/zfp 
 cd ..
 
 # 
@@ -36,10 +35,11 @@ cd ..
 #
 git clone  https://github.com/LLNL/H5Z-ZFP.git
 cd H5Z-ZFP 
-make CC=gcc ZFP_HOME=/opt/lib/zfp HDF5_HOME=/opt/lib/hdf5 PREFIX=/opt/lib/h5z_zfp 
-sudo make install
+make CC=gcc ZFP_HOME=/opt/lib/zfp HDF5_HOME=/opt/lib/hdf5 PREFIX=/opt/lib/h5z_zfp all
+sudo make  CC=gcc ZFP_HOME=/opt/lib/zfp HDF5_HOME=/opt/lib/hdf5 PREFIX=/opt/lib/h5z_zfp install
+cd ..
+echo "Auto Install Finished!"
 
-cd ~
 
 
 
