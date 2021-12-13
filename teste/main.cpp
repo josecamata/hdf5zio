18ce8bdffc39e08864a9a6e641f6dd6733c76bc8
Jose Camata <72111360+josecamata@users.noreply.github.com>
2021-12-13 16:19:40 -0300

    Update main.cpp

--- teste/main.cpp
+++ teste/main.cpp
@@ -1,1 +1,14 @@
 
+#include "joao_teste.h"
+#include "xdmf_teste.h"
+
+int main(void){
+
+    // * Teste padrão de compressão 1:
+    testePadrao();
+
+    // * Teste padrão de compressão 2 (com xdmf):
+    //testeXdmf();
+
+    return 0;
+}
