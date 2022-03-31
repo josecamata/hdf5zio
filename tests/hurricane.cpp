673fb47b79fb7aa0b8c8850313128b9ec1066049
Jose Camata <camata@ice.ufjf.br>
2022-03-31 16:57:03 -0300

    proximas etapas

--- tests/hurricane.cpp
+++ tests/hurricane.cpp
@@ -1,6 +1,18 @@
+
 int main(void){
 
+    // Ler os 48 arquivos QCLOUD no formanto bin
+    // Salvar no formato HDF5  (sem compressão, SZIP, ZLIP, ZFP)
+    // Gerar XDMF (HDF5+ZLIB) para visualizar no ParaView. 
     // * Teste padrão de compressão 1:
+
+    // Montar uma tabela de comparação:
+    // Metodo  |  Tamanho original |  Tamanho Comprimido | Taxa de Compressão
+    // sem  
+    // ZLIB
+    // SZIP
+    // ZFP (lossless)
+    // ZFP (precisão) 
 
     return 0;
 }
