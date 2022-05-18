32dab5922b0f1e0c70190c47a2547d95e9d3c5bc
Joao Victor Oliveira <joaovictoroliveira1452@gmail.com>
2022-05-17 23:59:26 -0300

    hurricane tests ok

--- tests/hurricane.cpp
+++ tests/hurricane.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <iostream>
+#include <string.h>
+#include <string>
 #include <iomanip>
 #include <fstream>
 #include <cmath>
+#include <ctime>
 
 #include "HDF5Writer.h"
 #include "HDF5Reader.h"
@@ -30,13 +33,12 @@ void read_hurricane_data(const char* file_path, float* dataset)
 
     archive.close();
 
-    std::cout<<std::boolalpha<<(cont == ny*nx*nz)<<std::endl;
+    // std::cout<<std::boolalpha<<(cont == ny*nx*nz)<<std::endl;
 
 }
 
 int main(int argc, char* argv[])
 {
-
     // Ler os 48 arquivos QCLOUD no formanto bin
     // Salvar no formato HDF5  (sem compressão, SZIP, ZLIP, ZFP)
     // Gerar XDMF (HDF5+ZLIB) para visualizar no ParaView. 
@@ -50,33 +52,73 @@ int main(int argc, char* argv[])
     // ZFP (lossless)
     // ZFP (precisão) 
 
-    float m = +INFINITY;
-    float M = -INFINITY;
+    std::ofstream log_file;
 
-    float* dataset = new float[500*500*100];
-    
+    // log_file.open(argv[4]);
+
+    // std::cout<<argv[4]<<std::endl;
+
+    std::clock_t c_start = std::clock();
+
+    float* dataset = new float[500*500*100]; //std::cout<<"\n\n"<<argv[1]<<std::endl;
+
     read_hurricane_data(argv[1], dataset);
 
-    for (size_t i = 0; i < 500*500*100; i++)
+    std::clock_t c_end = std::clock();
+
+    long time_elapsed_ms_read_to_dataset = 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC;
+
+    std::string file_name(argv[3]);
+    file_name = file_name.append(argv[2]);
+
+    const char* fileName = file_name.c_str();
+
+    c_start = std::clock();
+
+    if(strcmp(argv[5], "0") == 0)
     {
-        if(dataset[i] != 0)
-            std::cout<<dataset[i]<<std::endl;
-        if(dataset[i] > M)
-            M = dataset[i];
-        if(dataset[i] < m)
-            m = dataset[i];
+        //std::cout<<"78"<<std::endl;
+        HDF5Writer hdf_cloud(fileName);
+        hdf_cloud.write(dataset, 500*500*100, "dataset_cloud");
     }
+    else if(strcmp(argv[5], "1") == 0)
+    {
+        //std::cout<<"84"<<std::endl;
+        HDF5Writer hdf_cloud(fileName);
+        hdf_cloud.setCompression(SZIP); /* std::cout<<"---"<<std::endl; */
+        hdf_cloud.write(dataset, 500*500*100, "dataset_cloud_szip");
+    }
+    else if(strcmp(argv[5], "2") == 0)
+    {
+        //std::cout<<"91"<<std::endl;
+        HDF5Writer hdf_cloud(fileName);
+        hdf_cloud.setCompression(ZLIB); /* std::cout<<"---"<<std::endl; */
+        hdf_cloud.write(dataset, 500*500*100, "dataset_cloud_zlib");/* std::cout<<"---"<<std::endl; */
+    }
+    c_end = std::clock();
 
-    HDF5Writer hdf_cloud("dataset_cloud_24.h5");
-    hdf_cloud.write(dataset, 500*500*100, "dataset_cloud_24");
-    HDF5Writer hdf_cloud_z("dataset_cloud_24ZZZ.h5");
-    hdf_cloud_z.setCompression(ZLIB);
-    hdf_cloud_z.write(dataset, 500*500*100, "dataset_cloud_24");
+    // std::cout<<argv[4]<<std::endl;
 
-    delete dataset;
+    long time_elapsed_ms_write_to_dataset = 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC;
 
-    std::cout<<"\n\nM = "<<M<<std::endl;
-    std::cout<<"m = "<<m<<std::endl;
+    // HDF5Writer hdf_cloud("dataset_cloud_24.h5");
+    // hdf_cloud.write(dataset, 500*500*100, "dataset_cloud_24");
+    // HDF5Writer hdf_cloud_z("dataset_cloud_24ZZZ.h5");
+    // hdf_cloud_z.setCompression(ZLIB);
+    // hdf_cloud_z.write(dataset, 500*500*100, "dataset_cloud_24");
 
+    delete dataset;/* std::cout<<"106"<<std::endl; */
+
+    // log_file<<"Read dataset:\n"; std::cout<<"111"<<std::endl;
+    // log_file << "CPU time used: " << time_elapsed_ms_read_to_dataset << " ms\n";
+    // log_file << "CPU time used: " << time_elapsed_ms_read_to_dataset / 1000.0 << " s\n";
+
+    // log_file<<"Write dataset:\n";
+    // log_file << "CPU time used: " << time_elapsed_ms_write_to_dataset << " ms\n";
+    // log_file << "CPU time used: " << time_elapsed_ms_write_to_dataset / 1000.0 << " s\n";
+    // std::cout<<"115"<<std::endl;
     return 0;
 }
+
+//     0              1                       2                               3                    4                    5
+// ./hurricane [path para o raw] [nome de salvamento do arquivo] [path para salvar o arquivo] [arquivo de log] [tipo de compressao]
